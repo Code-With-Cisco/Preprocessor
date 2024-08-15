25116f55ca5f5cd423e92dbbffd945359e97d7c3
Cisco <115424057+Cisco305@users.noreply.github.com>
2024-08-15 15:53:38 -0400

    Initial commit

---
+++ test.java
@@ -0,0 +1,11 @@
+
+public class Test {
+    public static void main(String[] args) {
+        int x = 0;
+        if (x == 0) {
+            System.out.println("x is zero");
+        } else {
+            System.out.println("x is not zero");
+        }
+    }
+}
